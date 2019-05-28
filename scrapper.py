afa0a9efbeaedb0dd72030ad507c53bd2a9710bc
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-28 13:36:54 +0900

    get_season_ratings

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,6 @@ from urllib.request import urlopen
 
 SHOW_ID = "tt0944947"
 SHOW_URL = f'https://www.imdb.com/title/{SHOW_ID}/episodes'
-SEASONS = 0
 
 
 def get_seasons():
@@ -11,8 +10,25 @@ def get_seasons():
     html = seasons_page.read()
     seasons_page.close()
     seasons_bs = bs(html, "html.parser")
-    season_options = seasons_bs.find(id="bySeason").find_all("option")
-    SEASONS = season_options
+    season_count = seasons_bs.find(id="bySeason").find_all("option")
+    seasons = []
+    for i in range(1, len(season_count) + 1):
+        url = f'{SHOW_URL}?season={i}'
+        seasons.append(url)
+    return seasons
 
 
-get_seasons()
+def get_season_ratings(season_url):
+    season_page = urlopen(season_url)
+    html = season_page.read()
+    season_page.close()
+    season_bs = bs(html, "html.parser")
+    episodes = season_bs.find_all("div", class_="list_item")
+    for episode in episodes:
+        rating = episode.find("span", class_="ipl-rating-star__rating")
+        print(rating.string)
+
+
+seasons = get_seasons()
+for season in seasons:
+    get_season_ratings(season)
